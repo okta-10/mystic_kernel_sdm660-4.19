488db0d31a8a4cc1fd15378e360a97ce314ea22a
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-10-13 16:54:07 -0700

    mm/mmap: check on file instead of the rb_root_cached of its address_space
    In __vma_adjust(), we do the check on *root* to decide whether to adjust
    the address_space.  It seems to be more meaningful to do the check on
    *file* itself.  This means we are adjusting some data because it is a file
    backed vma.
    
    Since we seem to assume the address_space is valid if it is a file backed
    vma, let's just replace *root* with *file* here.
    
    Signed-off-by: Wei Yang <richard.weiyang@linux.alibaba.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: https://lkml.kernel.org/r/20200913133631.37781-2-richard.weiyang@linux.alibaba.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -891,7 +891,7 @@ again:
 			anon_vma_interval_tree_pre_update_vma(next);
 	}
 
-	if (root) {
+	if (file) {
 		flush_dcache_mmap_lock(mapping);
 		vma_interval_tree_remove(vma, root);
 		if (adjust_next)
@@ -912,7 +912,7 @@ again:
 		WRITE_ONCE(next->vm_pgoff, next->vm_pgoff + (adjust_next >> PAGE_SHIFT));
 	}
 
-	if (root) {
+	if (file) {
 		if (adjust_next)
 			vma_interval_tree_insert(next, root);
 		vma_interval_tree_insert(vma, root);
@@ -964,7 +964,7 @@ again:
 		anon_vma_unlock_write(anon_vma);
 	}
 
-	if (root) {
+	if (file) {
 		i_mmap_unlock_write(mapping);
 		uprobe_mmap(vma);
 
