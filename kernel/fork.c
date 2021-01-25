673cef1e1a476d619c6da5c9fc94764a2e37f9c1
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-26 03:24:54 +0530

    kernel: Boost to the max for a short amount of time when zygote forks
    In order to boost during the period from when a new app is forked to the
    time that it is added to the top-app cpuset (which is a signal to
    subsequently boost again), boost for a short amount of time when zygote
    forks. Not all processes forked from zygote are top apps, hence the
    decoupled boosting setup.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -97,6 +97,8 @@
 #include <linux/scs.h>
 #include <linux/devfreq_boost.h>
 #include <linux/simple_lmk.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/binfmts.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2372,6 +2374,7 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
+		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
