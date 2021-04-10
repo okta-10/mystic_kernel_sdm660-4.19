e44e0af26515683e58ac154afa2a7bc1e58a83b9
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-04-10 11:24:12 +0700

    Revert "ANDROID: net: bpf: permit redirect from ingress L3 to egress L2 devices at near max mtu"
    * for fix merge Linux 4.19.185
    
    This reverts commit 96ac927e5c7f759ec3c595042616cf67f9a33529.

--- net/core/filter.c
+++ net/core/filter.c
@@ -2838,9 +2838,8 @@ static int bpf_skb_net_shrink(struct sk_buff *skb, u32 len_diff)
 
 static u32 __bpf_skb_max_len(const struct sk_buff *skb)
 {
-	if (skb_at_tc_ingress(skb) || !skb->dev)
-		return SKB_MAX_ALLOC;
-	return skb->dev->mtu + skb->dev->hard_header_len;
+	return skb->dev ? skb->dev->mtu + skb->dev->hard_header_len :
+			  SKB_MAX_ALLOC;
 }
 
 static int bpf_skb_adjust_net(struct sk_buff *skb, s32 len_diff)
