6882c21c6af8ca06e06f4a8aaf6f626195262196
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-05-23 07:14:33 +0700

    Revert "mmc: quirks: add new quirk that allows Cache disable"
    * for fix merge Linux 4.19.191
    
    This reverts commit a073798a2b5b084ab175ad065010e4658a11d6ab.

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -1025,8 +1025,7 @@ int mmc_flush_cache(struct mmc_card *card)
 
 	if (mmc_card_mmc(card) &&
 			(card->ext_csd.cache_size > 0) &&
-			(card->ext_csd.cache_ctrl & 1) &&
-			(!(card->quirks & MMC_QUIRK_CACHE_DISABLE))) {
+			(card->ext_csd.cache_ctrl & 1)) {
 		err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
 				EXT_CSD_FLUSH_CACHE, 1, 0);
 		if (err)
